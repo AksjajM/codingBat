e163dcf135929caa1e5a70716e6012485a0021d0
Aks070 <113827304+Aks070@users.noreply.github.com>
2024-01-13 18:32:27 +0100

    Created the HaveThree method

--- src/exercise_3/HaveThree.java
+++ src/exercise_3/HaveThree.java
@@ -1,4 +1,14 @@
 package src.exercise_3;
 
 public class HaveThree {
+    public static boolean haveThree(int[] nums) {
+
+        int threeCounter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 3) threeCounter += 1;
+            if (i > 0 && nums[i] == 3 && nums[i - 1] == 3) return false;
+        }
+        return (threeCounter == 3);
+    }
 }
